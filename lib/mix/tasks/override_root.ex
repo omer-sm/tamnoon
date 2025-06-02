5c02fd58d5f3ac6fac2a77b50e3e629c9ab6bd5c
omer_sm <smorodinskyomer@gmail.com>
2025-06-02 15:46:45 +0300

    feat(override_root.ex): add override root task

---
+++ lib/mix/tasks/override_root.ex
@@ -0,0 +1,94 @@
+defmodule Mix.Tasks.Tamnoon.OverrideRoot do
+  @moduledoc """
+  Creates a default router module and a root component.
+  """
+  @shortdoc "Creates a default router module and a root component"
+
+  use Mix.Task
+
+  @impl Mix.Task
+  def run(_args) do
+    if !File.dir?("lib/components") do
+      if Mix.shell().yes?(
+           "Directory \"lib/components\" does not exist, do you want to run `mix tamnoon.make_dirs` to create it?",
+           default: :yes
+         ) do
+        Mix.Task.run("tamnoon.make_dirs")
+        Mix.Task.run("tamnoon.override_root")
+      end
+    else
+      app_name = Mix.Project.config()[:app] |> Atom.to_string() |> Macro.camelize()
+
+      if File.exists?("lib/router.ex") do
+        Mix.shell().info("File \"lib/router.ex\" already exists, skipping..")
+      else
+        File.write!("lib/router.ex", """
+        defmodule #{app_name}.Router do
+          @moduledoc \"\"\"
+          This module provides a default router for HTTP(S) requests, which builds and serves the web app.
+          \"\"\"
+
+          use Plug.Router
+
+          plug :match
+          plug :dispatch
+
+          get "/" do
+            Tamnoon.Compiler.build_from_root(#{app_name}.Components.Root)
+            send_file(conn, 200, "tamnoon_out/app.html")
+          end
+
+          get "/ws_connect.js" do
+            send_file(conn, 200, Application.app_dir(:tamnoon, "priv/static/ws_connect.js"))
+          end
+
+          match _ do
+            send_resp(conn, 404, "404")
+          end
+        end
+        """)
+
+        Mix.shell().info("Successfully created \"lib/router.ex\"!")
+      end
+
+      if File.exists?("lib/components/root.ex") do
+        Mix.shell().info("File \"lib/components/root.ex\" already exists, skipping..")
+      else
+        File.write!("lib/components/root.ex", """
+        defmodule #{app_name}.Components.Root do
+          @behaviour Tamnoon.Component
+
+          @impl true
+          def heex do
+            ~s\"\"\"
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+              <meta name="description" content="Webpage description goes here" />
+              <meta charset="utf-8">
+              <title>Tamnoon App</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <script src="ws_connect.js"></script>
+            </head>
+
+            <body>
+                <div class="app-container">
+                    <%= r.("app.html.heex") %>
+                </div>
+            </body>
+            </html>
+            \"\"\"
+          end
+        end
+        """)
+
+        Mix.shell().info("Successfully created \"lib/components/root.ex\"!")
+      end
+
+      Mix.shell().info(
+        "Done! Now add router: #{app_name}.Router to your Tamnoon options in `application.ex`."
+      )
+    end
+  end
+end
