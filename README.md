0cb5dc25763a78a38bd2716cc8e2f7f42247db0f
omer_sm <smorodinskyomer@gmail.com>
2025-07-11 12:21:18 +0300

    doc(docs): change version to rc.1

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The package can be installed by adding `tamnoon` to your list of dependencies in
 ```elixir
 def deps do
   [
-    {:tamnoon, "~> 1.0.0-a.5"}
+    {:tamnoon, "~> 1.0.0-rc.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tamnoon.MixProject do
   def project do
     [
       app: :tamnoon,
-      version: "1.0.0-a.5",
+      version: "1.0.0-rc.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
