15d738ac2b370368e4717d504b80ae8f17eeb2e5
omer_sm <smorodinskyomer@gmail.com>
2025-08-18 07:18:49 +0300

    fix(tamnoon_driver.js): fix attributes with name that includes their assigned value

--- priv/static/tamnoon_driver.js
+++ priv/static/tamnoon_driver.js
@@ -92,7 +92,7 @@ const applyDiffs = (diffs, rootElement = document, updateState = true) => {
           .filter((c) => c && new RegExp(`^tmnn-(?:not-)?${k}-`).test(c));
 
         classes.forEach((className) => {
-          const attr = className.slice(className.lastIndexOf(k) + 1 + k.length);
+          const attr = className.slice(className.indexOf(k) + 1 + k.length);
           const newValue = className.startsWith('tmnn-not-') ? !v : v;
 
           switch (attr) {
